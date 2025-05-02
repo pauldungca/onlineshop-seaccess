f2ffe9dc1d6f153bff6c72b6a3186ec14d9927cc
pauldungca <johnpauldungca0908@gmail.com>
2025-05-02 20:06:18 +0800

    Successfully completed part 2!

--- seaccess/routes/web.php
+++ seaccess/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\homeController;
 
 route::get('/', [homeController:: class, 'index'] );
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
